92ad6f543a3c3341b11f218f2d74ae279dcda378
AdibaAhmed07 <163102624+AdibaAhmed07@users.noreply.github.com>
2025-03-26 06:01:00 +0530

    updated main

---
+++ MainClass.java
@@ -0,0 +1,19 @@
+
+package Brick_Breaker;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        GamePanel gamePanel = new GamePanel();
+        frame.setSize(900, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("Brick Breaker Game");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gamePanel);
+    }
+}
